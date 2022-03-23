215855451580c37b8c9f401c58ad71cbdb541774
Bart Van Bos <bartvanbos@gmail.com>
2022-03-24 00:14:20 +0100

    wip

--- modules/management/main.tf
+++ modules/management/main.tf
@@ -21,6 +21,29 @@ resource "google_compute_instance" "zoo_navigator" {
 
 }
 
+# resource "google_compute_instance" "zk_web" {
+#   name         = "zk-web"
+#   machine_type = "g1-small"
+#   zone         = var.zone
+#   tags         = ["ssh", "http-server"]
+
+#   boot_disk {
+#     initialize_params {
+#       image = "gce-uefi-images/cos-stable"
+#     }
+#   }
+
+#   network_interface {
+#     subnetwork = var.subnet
+#     access_config {
+#       // Ephemeral IP
+#     }
+#   }
+
+#   metadata_startup_script = "docker run -d -p 80:8080 -e ZKWEB_PORT=8080 -e ZKWEB_CREDENTIALS=admin:admin -e ZKWEB_DEFAULT_NODE=zoo1:2181 --name zk-web --restart unless-stopped noteax/zk-web-docker:latest"
+
+# }
+
 resource "google_compute_instance" "kafka_drop" {
   name         = "kafka-drop"
   machine_type = "g1-small"
